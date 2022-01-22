967ce9081adac696de47328a92201769189d1243
Zhd68 <zhdanovalexey25@gmail.com>
2022-01-22 12:42:12 +0300

    Added 'private' attributes

--- soil.py
+++ soil.py
@@ -4,8 +4,8 @@ class Soil:
     удельного электрического споротивления грунта
     '''
     def __init__(self, soil_resistance):
-            self.soil_resistance = soil_resistance
+            self.__soil_resistance = soil_resistance
 
     def get_soil_resistance(self):
         '''Возвращает значение удельного электрического споротивления грунта'''
-        return self.soil_resistance
+        return self.__soil_resistance
